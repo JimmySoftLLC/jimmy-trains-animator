abeca1f0bcd11522c1e258c36dd8cc90b5f38208
Jimmysb <jbailey@jimmysoftllc.com>
2023-04-15 07:46:42 -0400

    added file methods

--- animator_lightning/files.py
+++ animator_lightning/files.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def print_directory(switch_0, path, tabs=0):
     print("Files on filesystem:")
@@ -29,3 +30,42 @@ def print_directory(switch_0, path, tabs=0):
         if isdir:
             print_directory(path + "/" + file, tabs + 1)
             
+def write_file_lines(file_name, lines):
+    with open(file_name, "w") as f:
+        for line in lines:
+            f.write(line + "\n")
+
+def read_file_lines(file_name):
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        output_lines = [] 
+        for line in lines:
+            output_lines.append(line.strip())
+        return output_lines
+    
+def write_file_line(file_name, line):
+    with open(file_name, "w") as f:
+        f.write(line + "\n")
+
+def read_file_line(file_name):
+    with open(file_name, "r") as f:
+        line = f.read()
+        output_line=line.strip()
+        return output_line
+    
+def json_stringify(python_dictionary):
+    json_string = json.dumps(python_dictionary)
+    return json_string
+
+def json_parse(my_object):
+    python_dictionary = json.loads(my_object)
+    return python_dictionary
+
+def write_json_file(file_name, python_dictionary):
+    json_string=json_stringify(python_dictionary)
+    write_file_line(file_name, json_string)
+    
+def read_json_file(file_name):
+    json_string=read_file_line(file_name)
+    python_dictionary=json_parse(json_string)
+    return python_dictionary
