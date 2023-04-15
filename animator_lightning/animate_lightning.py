9136551dd0a8bf2991f8b7f50bfc30f34c6ca884
Jimmysb <jbailey@jimmysoftllc.com>
2023-04-14 21:22:37 -0400

    refactoring

--- animator_lightning/animate_lightning.py
+++ animator_lightning/animate_lightning.py
@@ -1,74 +1,311 @@
+import time
+import audiocore
+import audiomixer
+import audiobusio
+import sdcardio
+import storage
+import busio
+import pwmio
+import digitalio
+import board
+import neopixel
 import random
+import rtc
 
-def lightning(ledStrip):
-    r = random.randint(40, 80)
-    g = random.randint(10, 25)
-    b = random.randint(0, 10)
+import audiomp3
+from analogio import AnalogIn
+from rainbowio import colorwheel
+from adafruit_debouncer import Debouncer
+import files
+import animate_lightning
 
-    # number of flashes
-    flashCount = random.randint (5, 10)
+################################################################################
+# Setup hardware
 
-    # flash white brightness range - 0-255
-    flashBrightnessMin =  10
-    flashBrightnessMax =  255
-    flashBrightness = random.randint(flashBrightnessMin, flashBrightnessMax) / 255
-    ledStrip.brightness = flashBrightness
+# Setup and analog pin to be used for volume control
+# the the volume control is digital by setting mixer voice levels
+analog_in = AnalogIn(board.A0)
+
+def get_voltage(pin):
+    return (pin.value) / 65536
+
+# Setup the switches, there are two the Left and Right or Black and Red
+SWITCH_1_PIN = board.GP6 #S1 on animator board
+SWITCH_2_PIN = board.GP7 #S2 on animator board
+
+switch_io_1 = digitalio.DigitalInOut(SWITCH_1_PIN)
+switch_io_1.direction = digitalio.Direction.INPUT
+switch_io_1.pull = digitalio.Pull.UP
+left_switch = Debouncer(switch_io_1)
+
+switch_io_2 = digitalio.DigitalInOut(SWITCH_2_PIN)
+switch_io_2.direction = digitalio.Direction.INPUT
+switch_io_2.pull = digitalio.Pull.UP
+right_switch = Debouncer(switch_io_2)
+
+# setup audio on the i2s bus, the animator uses the MAX98357A
+# the animator can have one or two MAX98357As. one for mono two for stereo
+# both MAX98357As share the same bus
+# for mono the MAX98357A defaults to combine channels
+# for stereo the MAX98357A SD pin is connected to VCC for right and a resistor to VCC for left
+# the audio mixer is used so that volume can be control digitally it is set to stereo
+# the sample_rate of the audio mixer is set to 22050 hz.  This is the max the raspberry pi pico can handle
+# all files with be in the wave format instead of mp3.  This eliminates the need for decoding
+i2s_bclk = board.GP18   # BCLK on MAX98357A
+i2s_lrc = board.GP19  # LRC on MAX98357A
+i2s_din = board.GP20  # DIN on MAX98357A
+
+audio = audiobusio.I2SOut(bit_clock=i2s_bclk, word_select=i2s_lrc, data=i2s_din)
+
+# Setup sdCard
+# the sdCard holds all the media and calibration files
+# if the card is missing a voice command is spoken
+# the user inserts the card a presses the left button to move forward
+sck = board.GP2
+si = board.GP3
+so = board.GP4
+cs = board.GP5
+spi = busio.SPI(sck, si, so)
+try:
+  sdcard = sdcardio.SDCard(spi, cs)
+  vfs = storage.VfsFat(sdcard)
+  storage.mount(vfs, "/sd")
+except:
+  wave0 = audiomp3.MP3Decoder(open("wav/micro_sd_card_not_inserted.mp3", "rb"))
+  audio.play(wave0)
+  while audio.playing:
+    pass
+  cardInserted = False
+  while not cardInserted:
+    left_switch.update()
+    if left_switch.fell:
+        try:
+            sdcard = sdcardio.SDCard(spi, cs)
+            vfs = storage.VfsFat(sdcard)
+            storage.mount(vfs, "/sd")
+            cardInserted = True
+            wave0 = audiomp3.MP3Decoder(open("wav/micro_sd_card_success.mp3", "rb"))
+            audio.play(wave0)
+            while audio.playing:
+                pass
+        except:
+            wave0 = audiomp3.MP3Decoder(open("wav/micro_sd_card_not_inserted.mp3", "rb"))
+            audio.play(wave0)
+            while audio.playing:
+                pass
+
+# Setup the mixer it can play higher quality audio wav using larger wave files
+# wave files are less cpu intensive since they are not compressed
+num_voices = 2
+mixer = audiomixer.Mixer(voice_count=num_voices, sample_rate=22050, channel_count=2,
+                         bits_per_sample=16, samples_signed=True)
+audio.play(mixer)
+
+# Setup time
+r = rtc.RTC()
+r.datetime = time.struct_time((2019, 5, 29, 15, 14, 15, 0, -1, -1))
+
+#setup neo pixels
+num_pixels = 30
+ledStrip = neopixel.NeoPixel(board.GP10, num_pixels)
+ledStrip.auto_write = False
+ledStrip.brightness = 1.0
+
+################################################################################
+# Global Variables
+
+main_menu = ['sound_options','calibrate_position']
+
+feller_sound_options = ['option_birds_dogs','option_music','option_no_sounds','option_random','option_thunder']
+
+################################################################################
+# Global Methods
+
+def setVolume():
+    volume = get_voltage(analog_in)
+    mixer.voice[0].level = volume
+    mixer.voice[1].level = volume
     
-    #print (str(time.monotonic()-startTime))
+def sleepAndUpdateVolume(seconds):
+    setVolume()
+    time.sleep(seconds)
 
-    # flash duration range - ms
-    flashDurationMin = 5
-    flashDurationMax = 75
+################################################################################
+# State Machine
 
-    # flash off range - ms
-    flashOffsetMin = 0
-    flashOffsetMax = 75
+class StateMachine(object):
 
-    # time to next flash range - ms
-    nextFlashDelayMin = 1
-    nextFlashDelayMax = 50
+    def __init__(self):
+        self.state = None
+        self.states = {}
+        self.paused_state = None
+
+    def add_state(self, state):
+        self.states[state.name] = state
+
+    def go_to_state(self, state_name):
+        if self.state:
+            self.state.exit(self)
+        self.state = self.states[state_name]
+        self.state.enter(self)
+
+    def update(self):
+        if self.state:
+            self.state.update(self)
+
+    # When pausing, don't exit the state
+    def pause(self):
+        self.state = self.states['paused']
+        self.state.enter(self)
+
+    # When resuming, don't re-enter the state
+    def resume_state(self, state_name):
+        if self.state:
+            self.state.exit(self)
+        self.state = self.states[state_name]
+
+    def reset(self):
+        """As indicated, reset"""
+        self.firework_color = random_color()
+        self.burst_count = 0
+        self.shower_count = 0
+        self.firework_step_time = time.monotonic() + 0.05
+
+################################################################################
+# States
+
+# Abstract parent state class.
+class State(object):
+
+    def __init__(self):
+        pass
+
+    @property
+    def name(self):
+        return ''
+
+    def enter(self, machine):
+        pass
+
+    def exit(self, machine):
+        pass
+
+    def update(self, machine):
+        if left_switch.fell:
+            machine.paused_state = machine.state.name
+            machine.pause()
+            return False
+        return True
+
+class WaitingState(State):
+
+    def __init__(self):      
+        pass
+
+    @property
+    def name(self):
+        return 'waiting'
+
+    def enter(self, machine):
+        State.enter(self, machine)
+
+    def exit(self, machine):
+        State.exit(self, machine)
+
+    def update(self, machine):
+        left_switch.update()
+        right_switch.update()
+        if left_switch.fell:
+            animate_lightning.animation_one(sleepAndUpdateVolume, audiocore, mixer, ledStrip, left_switch)
+        if right_switch.fell:
+            print('Just pressed 1')
+            machine.go_to_state('program')
+
+class ProgramState(State):
+
+    def __init__(self):
+        self.optionIndex = 0
+
+    @property
+    def name(self):
+        return 'program'
+
+    def enter(self, machine):
+        print('Select a program option')
+        if mixer.voice[0].playing:
+            mixer.voice[0].stop()
+            while mixer.voice[0].playing:
+                pass
+        else:
+            wave0 = audiocore.WaveFile(open("/sd/feller_confirmations/option_mode_entered.wav", "rb"))
+            mixer.voice[0].play( wave0, loop=False )
+            while mixer.voice[0].playing:
+                pass
+        State.enter(self, machine)
+
+    def exit(self, machine):
+        State.exit(self, machine)
+
+    def update(self, machine):
+        left_switch.update()
+        right_switch.update()
+        if left_switch.fell:
+            if mixer.voice[0].playing:
+                mixer.voice[0].stop()
+                while mixer.voice[0].playing:
+                    pass
+            else:
+                wave0 = audiocore.WaveFile(open("/sd/feller_sound_options/" + feller_sound_options[self.optionIndex] + ".wav" , "rb"))
+                mixer.voice[0].play( wave0, loop=False )
+                self.optionIndex +=1
+                if self.optionIndex > 4:
+                    self.optionIndex = 0
+                while mixer.voice[0].playing:
+                    pass
+        if right_switch.fell:
+            if mixer.voice[0].playing:
+                mixer.voice[0].stop()
+                while mixer.voice[0].playing:
+                    pass
+            else:
+                wave0 = audiocore.WaveFile(open("/sd/feller_confirmations/option_selected.wav", "rb"))
+                mixer.voice[0].play( wave0, loop=False )
+                while mixer.voice[0].playing:
+                    pass
+            machine.go_to_state('waiting')
+
+# StateTemplate copy and add functionality
+class StateTemplate(State):
+
+    def __init__(self):
+        super().__init__()
+
+    @property
+    def name(self):
+        return 'example'
+
+    def enter(self, machine):
+        State.enter(self, machine)
+
+    def exit(self, machine):
+        State.exit(self, machine)
+
+    def update(self, machine):
+        State.update(self, machine)
+
+
+###############################################################################
+# Create the state machine
+
+pretty_state_machine = StateMachine()
+pretty_state_machine.add_state(WaitingState())
+pretty_state_machine.add_state(ProgramState())
+
+print("animator has started")
+
+pretty_state_machine.go_to_state('waiting')
+
+while True:
+    pretty_state_machine.update()
+    sleepAndUpdateVolume(.1)
     
-    for i in range(0,flashCount):
-        color = random.randint(0, 50)
-        if color < 0: color = 0
-        
-        ledStrip.fill((r + color, g + color, b + color))
-        ledStrip.show()
-        delay = random.randint(flashOffsetMin, flashOffsetMax)
-        delay = delay/1000
-        time.sleep(delay)
-        ledStrip.fill((0, 0, 0))
-        ledStrip.show()
-        
-        ledStrip.fill((r + color, g + color, b + color))
-        ledStrip.show()
-        delay = random.randint(flashOffsetMin, flashOffsetMax)
-        delay = delay/1000
-        time.sleep(delay)
-        ledStrip.fill((0, 0, 0))
-        ledStrip.show()
-        
-        ledStrip.fill((r + color, g + color, b + color))
-        ledStrip.show();
-        delay = random.randint(flashOffsetMin, flashOffsetMax)
-        delay = delay/1000
-        time.sleep(delay)
-        ledStrip.fill((0, 0, 0))
-        ledStrip.show()
-        
-        ledStrip.fill((r + color, g + color, b + color))
-        ledStrip.show()
-        delay = random.randint(flashOffsetMin, flashOffsetMax)
-        delay = delay/1000
-        time.sleep(delay)
-        ledStrip.fill((0, 0, 0))
-        ledStrip.show()
-        
-        delay = random.randint(nextFlashDelayMin, nextFlashDelayMax)
-        delay = delay/1000
-        time.sleep(delay)
-        ledStrip.fill((0, 0, 0))
-        ledStrip.show()
-
-def animation_one(sleepAndUpdateVolume, audiocore, mixer, ledStrip):
-    lightning(ledStrip)
