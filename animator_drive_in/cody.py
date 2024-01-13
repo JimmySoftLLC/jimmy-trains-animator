5938e19e316b42ced8c4abcec00fddc79d299e21
Jimmysb <jbailey@jimmysoftllc.com>
2024-01-13 14:02:44 -0500

    working code

--- animator_drive_in/cody.py
+++ animator_drive_in/cody.py
@@ -1,52 +1,27 @@
-import RPi.GPIO as GPIO
-import os
-import sys
 import vlc
 import time
 
 movie1 = ("/home/pi/Videos/oldmovie.mp4")
-movie2 = ("/home/pi/Videos/Feller.mp4")
-movie3 = ("/home/pi/Videos/jimmytrains.jpg")
-movie4 = ("/home/pi/Videos/grand_central.mp4")
-movie5 = ("/home/pi/Videos/commercial.mp4")
+movie2 = ("/home/pi/Videos/commercial.mp4")
 
 # creating vlc media player object
 media_player = vlc.MediaPlayer()
 media_player.toggle_fullscreen()
+media_player.audio_set_volume(50)
 
 for _ in range(3):
     # set media and play
     media = vlc.Media(movie1)
     media_player.set_media(media)
     media_player.play()
-    time.sleep(2)
+    time.sleep(100)
     media_player.pause()
 
     # set media and play
     media = vlc.Media(movie2)
     media_player.set_media(media)
     media_player.play()
-    time.sleep(5)
-    media_player.pause()
-
-    # set media and play
-    media = vlc.Media(movie3)
-    media_player.set_media(media)
-    time.sleep(2)
-    media_player.pause()
-
-    # set media and play
-    media = vlc.Media(movie4)
-    media_player.set_media(media)
-    media_player.play()
-    time.sleep(2)
-    media_player.pause()
-
-    # set media and play
-    media = vlc.Media(movie5)
-    media_player.set_media(media)
-    media_player.play()
-    time.sleep(10)
+    time.sleep(100)
     media_player.pause()
 
 media_player.stop()
