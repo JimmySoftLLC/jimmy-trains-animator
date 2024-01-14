fae7f72c00508768dca3243c1858ee99fad03994
Jimmysb <jbailey@jimmysoftllc.com>
2024-01-14 17:54:18 -0500

    tested out board

--- animator_drive_in/cody.py
+++ animator_drive_in/cody.py
@@ -5,22 +5,62 @@ import digitalio
 from adafruit_debouncer import Debouncer
 import neopixel_spi
 from rainbowio import colorwheel
+import pwmio
+from adafruit_motor import servo
 
-# Setup the switches, there are two the Left and Right
+# Setup the switches, there are two the Left and Right and two unused 3 and 4
 switch_io_1 = digitalio.DigitalInOut(board.D17)
 switch_io_1.direction = digitalio.Direction.INPUT
 switch_io_1.pull = digitalio.Pull.UP
-left_switch = Debouncer(switch_io_1)
 
 switch_io_2 = digitalio.DigitalInOut(board.D27)
 switch_io_2.direction = digitalio.Direction.INPUT
 switch_io_2.pull = digitalio.Pull.UP
+
+switch_io_3 = digitalio.DigitalInOut(board.D22)
+switch_io_3.direction = digitalio.Direction.INPUT
+switch_io_3.pull = digitalio.Pull.UP
+
+switch_io_4 = digitalio.DigitalInOut(board.D5)
+switch_io_4.direction = digitalio.Direction.INPUT
+switch_io_4.pull = digitalio.Pull.UP
+
+left_switch = Debouncer(switch_io_1)
 right_switch = Debouncer(switch_io_2)
 
+# Setup the servo
+m1_pwm = pwmio.PWMOut(board.D6, duty_cycle=2 ** 15, frequency=50)
+m2_pwm = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50)
+m3_pwm = pwmio.PWMOut(board.D23, duty_cycle=2 ** 15, frequency=50)
+m4_pwm = pwmio.PWMOut(board.D24, duty_cycle=2 ** 15, frequency=50)
+m5_pwm = pwmio.PWMOut(board.D25, duty_cycle=2 ** 15, frequency=50)
+m6_pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50)
+m7_pwm = pwmio.PWMOut(board.D16, duty_cycle=2 ** 15, frequency=50)
+m8_pwm = pwmio.PWMOut(board.D20, duty_cycle=2 ** 15, frequency=50)
+
+m1_servo = servo.Servo(m1_pwm)
+m2_servo = servo.Servo(m2_pwm)
+m3_servo = servo.Servo(m3_pwm)
+m4_servo = servo.Servo(m4_pwm)
+m5_servo = servo.Servo(m5_pwm)
+m6_servo = servo.Servo(m6_pwm)
+m7_servo = servo.Servo(m7_pwm)
+m8_servo = servo.Servo(m8_pwm)
+
+m1_servo.angle = 180
+m2_servo.angle = 180
+m3_servo.angle = 180
+m4_servo.angle = 180
+m5_servo.angle = 180
+m6_servo.angle = 180
+m7_servo.angle = 180
+m8_servo.angle = 180
+
+# setup neopixels on spi GP10
 num_pixels_rgb=40
 ledStripRGB = neopixel_spi.NeoPixel_SPI(board.SPI(), num_pixels_rgb, brightness=1.0, auto_write=False)
 
-# creating vlc media player object
+# create vlc media player object for playing video, music etc
 media_player = vlc.MediaPlayer()
 media_player.toggle_fullscreen()
   
@@ -89,4 +129,3 @@ while True:
     pass
 
 media_player.stop()      
-
