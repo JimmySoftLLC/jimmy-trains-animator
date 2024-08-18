2c855e590bb31e3c4ccc90714fb24ff183eb78a9
James Bailey <jbailey@jimmysoftllc.com>
2024-08-18 13:32:58 -0400

    added asyncio to flag

--- animator_boy_with_kite/code.py
+++ animator_boy_with_kite/code.py
@@ -18,8 +18,9 @@ import gc
 def gc_col(collection_point):
     gc.collect()
     start_mem = gc.mem_free()
-    files.log_item("Point " + collection_point +
-                   " Available memory: {} bytes".format(start_mem))
+    files.log_item(
+        "Point " + collection_point + " Available memory: {} bytes".format(start_mem)
+    )
 
 
 gc_col("Imports gc, files")
@@ -94,8 +95,14 @@ din = board.GP20  # DIN on MAX98357A
 aud = audiobusio.I2SOut(bit_clock=bclk, word_select=lrc, data=din)
 
 # Setup the mixer to play mp3 files
-mix = audiomixer.Mixer(voice_count=1, sample_rate=22050, channel_count=2,
-                       bits_per_sample=16, samples_signed=True, buffer_size=16384)
+mix = audiomixer.Mixer(
+    voice_count=1,
+    sample_rate=22050,
+    channel_count=2,
+    bits_per_sample=16,
+    samples_signed=True,
+    buffer_size=16384,
+)
 
 aud.play(mix)
 
@@ -109,22 +116,24 @@ def upd_vol(s):
         try:
             v = int(cfg["volume"]) / 100
         except:
-            v = .5
+            v = 0.5
         if v < 0 or v > 1:
-            v = .5
+            v = 0.5
         mix.voice[0].level = v
         time.sleep(s)
+
 
 upd_vol(0.01)
 
 # Setup the servos
-kite_rot = pwmio.PWMOut(board.GP17, duty_cycle=2 ** 15, frequency=50)
+kite_rot = pwmio.PWMOut(board.GP17, duty_cycle=2**15, frequency=50)
 kite_rot = servo.Servo(kite_rot, min_pulse=500, max_pulse=2500)
 kite_rot.angle = lst_kite_rot_pos
 
 
 ################################################################################
 # sound
+
 
 def ply_a_0(file_name):
     upd_vol(0.01)
@@ -170,6 +179,7 @@ def ch_vol(action):
     ply_a_0("volume")
     spk_word(cfg["volume"])
 
+
 def spk_sentence(snd):
     print(snd)
     try:
@@ -179,33 +189,36 @@ def spk_sentence(snd):
         for snd in snd_split:
             spk_word(snd)
 
+
 def spk_word(str_to_speak):
     print(str_to_speak)
-    if (str_to_speak == "minute" or 
-        str_to_speak == "minutes" or 
-        str_to_speak == "timer" or 
-        str_to_speak == "lower" or 
-        str_to_speak == "raise" or 
-        str_to_speak == "no" or
-        str_to_speak == "continuous" or 
-        str_to_speak == "options" or 
-        str_to_speak == "this" or 
-        str_to_speak == "exit" or 
-        str_to_speak == "settings" or 
-        str_to_speak == "main" or 
-        str_to_speak == "menu" or 
-        str_to_speak == "adjustment" or 
-        str_to_speak == "volume" or 
-        str_to_speak == "pot" or 
-        str_to_speak == "off" or
-        str_to_speak == "on" or
-        str_to_speak == "random" or
-        str_to_speak == "to" or
-        str_to_speak == "lowerraisesavevol" or
-        str_to_speak == "mode" or
-        str_to_speak == "centerfig" or
-        str_to_speak == "alignlrsave" or
-        str_to_speak == "wind"):
+    if (
+        str_to_speak == "minute"
+        or str_to_speak == "minutes"
+        or str_to_speak == "timer"
+        or str_to_speak == "lower"
+        or str_to_speak == "raise"
+        or str_to_speak == "no"
+        or str_to_speak == "continuous"
+        or str_to_speak == "options"
+        or str_to_speak == "this"
+        or str_to_speak == "exit"
+        or str_to_speak == "settings"
+        or str_to_speak == "main"
+        or str_to_speak == "menu"
+        or str_to_speak == "adjustment"
+        or str_to_speak == "volume"
+        or str_to_speak == "pot"
+        or str_to_speak == "off"
+        or str_to_speak == "on"
+        or str_to_speak == "random"
+        or str_to_speak == "to"
+        or str_to_speak == "lowerraisesavevol"
+        or str_to_speak == "mode"
+        or str_to_speak == "centerfig"
+        or str_to_speak == "alignlrsave"
+        or str_to_speak == "wind"
+    ):
         ply_a_0(str_to_speak)
         return
     for character in str_to_speak:
@@ -214,6 +227,7 @@ def spk_word(str_to_speak):
         except Exception as e:
             files.log_item(e)
             print("Invalid character in string to speak")
+
 
 ################################################################################
 # misc
@@ -226,6 +240,7 @@ def exit_early():
         kill_process = True
         mix.voice[0].stop()
 
+
 ################################################################################
 # motors
 
@@ -234,14 +249,14 @@ step_down = [
     [0, 0, 1, 1],  # Step 1
     [0, 1, 1, 0],  # Step 2
     [1, 1, 0, 0],  # Step 3
-    [1, 0, 0, 1]   # Step 4
+    [1, 0, 0, 1],  # Step 4
 ]
 
 step_up = [
     [1, 0, 0, 1],  # Step 4
     [1, 1, 0, 0],  # Step 3
     [0, 1, 1, 0],  # Step 2
-    [0, 0, 1, 1]   # Step 1
+    [0, 0, 1, 1],  # Step 1
 ]
 
 
@@ -268,6 +283,7 @@ def servo_m(servo_pos):
     kite_rot.angle = servo_pos
     lst_kite_rot_pos = servo_pos
 
+
 def ch_servo(action):
     s = int(cfg["servo"])
     if "servo" in action:
@@ -285,6 +301,7 @@ def ch_servo(action):
     servo_m(int(cfg["servo"]))
     spk_word(cfg["servo"])
 
+
 ################################################################################
 # async methods
 
@@ -298,55 +315,55 @@ def rotate_spd():
     else:
         return 0.02
 
-                                                                                                                                                                                                                                                                                
+
 async def rotate_kite_async():
     global lst_kite_rot_pos, async_running
     while async_running:
         center_servo_pos = int(cfg["servo"])
-        rand_pos_1 = random.randint(center_servo_pos-70, center_servo_pos-70)
-        rand_pos_2 = random.randint(center_servo_pos+70, center_servo_pos+70)
+        rand_pos_1 = random.randint(center_servo_pos - 70, center_servo_pos - 70)
+        rand_pos_2 = random.randint(center_servo_pos + 70, center_servo_pos + 70)
         sign = 1
         if lst_kite_rot_pos > rand_pos_1:
             sign = -1
         total_steps = abs(rand_pos_1 - lst_kite_rot_pos)
         exit_early()
         if not async_running or kill_process:
-                break
+            break
         for _ in range(total_steps + 1):
             spd = rotate_spd()
             kite_ang = lst_kite_rot_pos + 1 * sign
             servo_m(kite_ang)
             await asyncio.sleep(spd)
-        await asyncio.sleep(2*spd)
+        await asyncio.sleep(2 * spd)
         sign = 1
         if lst_kite_rot_pos > rand_pos_2:
             sign = -1
         total_steps = abs(rand_pos_2 - lst_kite_rot_pos)
         exit_early()
         if not async_running or kill_process:
-                break
+            break
         for _ in range(total_steps + 1):
             spd = rotate_spd()
             kite_ang = lst_kite_rot_pos + 1 * sign
             servo_m(kite_ang)
             await asyncio.sleep(spd)
-        await asyncio.sleep(2*spd)
+        await asyncio.sleep(2 * spd)
 
 
 async def deploy_kite(steps, direction, spd=0.005):
     global async_running, lst_kite_deploy_pos
-    if direction == 'down':
+    if direction == "down":
         seq = step_down
-    elif direction == 'up':
+    elif direction == "up":
         seq = step_up
     else:
         raise ValueError("Direction must be 'down' or 'up'")
-    for i in range(steps):
+    for _ in range(steps):
         if kill_process:
             break
-        if direction == 'down':
+        if direction == "down":
             lst_kite_deploy_pos -= 1
-        elif direction == 'up':
+        elif direction == "up":
             lst_kite_deploy_pos += 1
         for step in seq:
             set_step(step)
@@ -361,6 +378,7 @@ async def rn_an(steps, direction):
     deploy_g = asyncio.create_task(deploy_kite(steps, direction))
     await asyncio.gather(deploy_g, rot_k)
 
+
 ################################################################################
 # Animations
 
@@ -370,6 +388,7 @@ def rnd_prob(c):
     if y < c:
         return True
     return False
+
 
 def an():
     global kill_process
@@ -382,11 +401,11 @@ def an():
     for _ in range(cycles):
         if kill_process:
             coils_off()
-            return 
-        if rnd_prob(.2) and not mix.voice[0].playing and cfg["wind"] == True:
+            return
+        if rnd_prob(0.2) and not mix.voice[0].playing and cfg["wind"] == True:
             mix.voice[0].play(w0, loop=False)
         if cfg["random"] == True:
-            rand_deploy_pos = random.randint(0, kite_deploy_max) 
+            rand_deploy_pos = random.randint(0, kite_deploy_max)
             direction = "up"
             if lst_kite_deploy_pos > rand_deploy_pos:
                 direction = "down"
@@ -402,10 +421,11 @@ def an():
     total_steps = abs(0 - lst_kite_deploy_pos)
     asyncio.run(rn_an(total_steps, "down"))
     coils_off()
-        
-        
+
+
 ################################################################################
 # State Machine
+
 
 class StMch(object):
 
@@ -427,6 +447,7 @@ class StMch(object):
         if s.ste:
             s.ste.upd(s)
 
+
 ################################################################################
 # States
 
@@ -440,7 +461,7 @@ class Ste(object):
 
     @property
     def name(s):
-        return ''
+        return ""
 
     def enter(s, mch):
         pass
@@ -460,7 +481,7 @@ class BseSt(Ste):
 
     @property
     def name(self):
-        return 'base_state'
+        return "base_state"
 
     def enter(self, mch):
         # set servos to starting position
@@ -472,29 +493,28 @@ class BseSt(Ste):
         Ste.exit(self, mch)
 
     def upd(self, mch):
-        global cont_run, fig_web,rand_timer
-        sw = utilities.switch_state(
-            l_sw, r_sw, upd_vol, 3.0)
+        global cont_run, fig_web, rand_timer
+        sw = utilities.switch_state(l_sw, r_sw, upd_vol, 3.0)
         if sw == "left_held":
-            if cfg["timer"]==True:
-               cfg["timer"] = False
-               cont_run = False
-               aud_en.value = False
-               files.write_json_file("cfg.json", cfg)
-               aud_en.value = True
-               spk_sentence("timer_mode_off")
-               return
+            if cfg["timer"] == True:
+                cfg["timer"] = False
+                cont_run = False
+                aud_en.value = False
+                files.write_json_file("cfg.json", cfg)
+                aud_en.value = True
+                spk_sentence("timer_mode_off")
+                return
             if cont_run:
                 cont_run = False
                 spk_sentence("continuous_mode_off")
             elif cfg["timer"] == False:
                 cont_run = True
                 spk_sentence("continuous_mode_on")
-        elif cfg["timer"]==True:
+        elif cfg["timer"] == True:
             if rand_timer <= 0:
                 an()
                 coils_off()
-                rand_timer = int(cfg["timer_val"])*60
+                rand_timer = int(cfg["timer_val"]) * 60
                 print("an done")
             else:
                 upd_vol(1)
@@ -503,7 +523,7 @@ class BseSt(Ste):
             an()
             print("an done")
         elif sw == "right":
-            mch.go_to('main_menu')
+            mch.go_to("main_menu")
 
 
 class Main(Ste):
@@ -514,10 +534,10 @@ class Main(Ste):
 
     @property
     def name(self):
-        return 'main_menu'
+        return "main_menu"
 
     def enter(self, mch):
-        files.log_item('Main menu')
+        files.log_item("Main menu")
         spk_sentence("main_menu")
         spk_sentence("r_l_but")
         Ste.enter(self, mch)
@@ -532,19 +552,19 @@ class Main(Ste):
             spk_sentence(main_m[self.i])
             self.sel_i = self.i
             self.i += 1
-            if self.i > len(main_m)-1:
+            if self.i > len(main_m) - 1:
                 self.i = 0
         if r_sw.fell:
             sel_i = main_m[self.sel_i]
             if sel_i == "options":
-                mch.go_to('options')
+                mch.go_to("options")
             elif sel_i == "volume_settings":
-                mch.go_to('volume_settings')
+                mch.go_to("volume_settings")
             elif sel_i == "centerfig":
-                mch.go_to('servo_settings')    
+                mch.go_to("servo_settings")
             else:
                 ply_a_0("all_changes_complete")
-                mch.go_to('base_state')
+                mch.go_to("base_state")
 
 
 class VolSet(Ste):
@@ -555,10 +575,10 @@ class VolSet(Ste):
 
     @property
     def name(self):
-        return 'volume_settings'
+        return "volume_settings"
 
     def enter(self, mch):
-        files.log_item('Set Web Options')
+        files.log_item("Set Web Options")
         spk_sentence("volume_settings_menu")
         spk_sentence("r_l_but")
         Ste.enter(self, mch)
@@ -573,7 +593,7 @@ class VolSet(Ste):
             spk_sentence(v_set[self.i])
             self.sel_i = self.i
             self.i += 1
-            if self.i > len(v_set)-1:
+            if self.i > len(v_set) - 1:
                 self.i = 0
         if r_sw.fell:
             sel_mnu = v_set[self.sel_i]
@@ -581,20 +601,18 @@ class VolSet(Ste):
                 spk_sentence("volume_adjustment_menu_lowerraisesavevol")
                 done = False
                 while not done:
-                    sw = utilities.switch_state(
-                        l_sw, r_sw, upd_vol, 3.0)
+                    sw = utilities.switch_state(l_sw, r_sw, upd_vol, 3.0)
                     if sw == "left":
                         ch_vol("lower")
                     elif sw == "right":
                         ch_vol("raise")
                     elif sw == "right_held":
                         aud_en.value = False
-                        files.write_json_file(
-                            "cfg.json", cfg)
+                        files.write_json_file("cfg.json", cfg)
                         aud_en.value = True
                         ply_a_0("all_changes_complete")
                         done = True
-                        mch.go_to('base_state')
+                        mch.go_to("base_state")
                     pass
             elif sel_mnu == "volume_pot_off":
                 cfg["volume_pot"] = False
@@ -604,15 +622,14 @@ class VolSet(Ste):
                 files.write_json_file("cfg.json", cfg)
                 aud_en.value = True
                 ply_a_0("all_changes_complete")
-                mch.go_to('base_state')
+                mch.go_to("base_state")
             elif sel_mnu == "volume_pot_on":
                 cfg["volume_pot"] = True
                 aud_en.value = False
                 files.write_json_file("cfg.json", cfg)
                 aud_en.value = True
                 ply_a_0("all_changes_complete")
-                mch.go_to('base_state')
-
+                mch.go_to("base_state")
 
 
 class Opt(Ste):
@@ -623,10 +640,10 @@ class Opt(Ste):
 
     @property
     def name(self):
-        return 'options'
+        return "options"
 
     def enter(self, mch):
-        files.log_item('Choose sounds menu')
+        files.log_item("Choose sounds menu")
         spk_sentence("options_menu")
         spk_sentence("r_l_but")
         Ste.enter(self, mch)
@@ -642,43 +659,45 @@ class Opt(Ste):
             spk_sentence(mnu_o[self.i])
             self.sel_i = self.i
             self.i += 1
-            if self.i > len(mnu_o)-1:self.i = 0
+            if self.i > len(mnu_o) - 1:
+                self.i = 0
         if r_sw.fell:
             options = mnu_o[self.sel_i].split("_")
-            if options[0]=="timer":
+            if options[0] == "timer":
                 cfg["timer"] = True
                 cfg["timer_val"] = str(options[1])
                 rand_timer = 0
-            elif mnu_o[self.sel_i]=="wind":
+            elif mnu_o[self.sel_i] == "wind":
                 cfg["wind"] = True
-            elif mnu_o[self.sel_i]=="no_wind":
+            elif mnu_o[self.sel_i] == "no_wind":
                 cfg["wind"] = False
-            elif mnu_o[self.sel_i]=="random_raise_lower":
+            elif mnu_o[self.sel_i] == "random_raise_lower":
                 cfg["random"] = True
-            elif mnu_o[self.sel_i]=="raise_lower":
+            elif mnu_o[self.sel_i] == "raise_lower":
                 cfg["random"] = False
-            elif mnu_o[self.sel_i]=="exit_this_menu":
+            elif mnu_o[self.sel_i] == "exit_this_menu":
                 aud_en.value = False
                 files.write_json_file("cfg.json", cfg)
                 aud_en.value = True
                 ply_a_0("all_changes_complete")
-                mch.go_to('base_state')
+                mch.go_to("base_state")
                 return
             ply_a_0("option_set")
 
+
 class ServoSet(Ste):
-    
+
     def __init__(self):
         self.i = 0
         self.sel_i = 0
 
     @property
     def name(self):
-        return 'servo_settings'
+        return "servo_settings"
 
     def enter(self, mch):
         global kill_process
-        files.log_item('Set Web Options')
+        files.log_item("Set Web Options")
         spk_sentence("centerfig_menu")
         spk_sentence("alignlrsave")
         cfg["servo"] = 90
@@ -694,20 +713,18 @@ class ServoSet(Ste):
         r_sw.update()
         done = False
         while not done:
-            sw = utilities.switch_state(
-                l_sw, r_sw, upd_vol, 3.0)
+            sw = utilities.switch_state(l_sw, r_sw, upd_vol, 3.0)
             if sw == "left":
                 ch_servo("left")
             elif sw == "right":
                 ch_servo("right")
             elif sw == "right_held":
                 aud_en.value = False
-                files.write_json_file(
-                    "cfg.json", cfg)
+                files.write_json_file("cfg.json", cfg)
                 aud_en.value = True
                 ply_a_0("all_changes_complete")
                 done = True
-                mch.go_to('base_state')
+                mch.go_to("base_state")
             pass
 
 
@@ -724,13 +741,10 @@ st_mch.add(ServoSet())
 aud_en.value = True
 
 upd_vol(0.01)
-st_mch.go_to('base_state')
+st_mch.go_to("base_state")
 files.log_item("animator has started...")
 gc_col("animations started")
 
 while True:
     st_mch.upd()
     upd_vol(0.01)
-
-
-
