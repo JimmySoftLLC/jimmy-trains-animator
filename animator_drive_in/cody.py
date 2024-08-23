647fc23c789af1cd556b4a22bf11debc23d086d5
Jimmysb <jbailey@jimmysoftllc.com>
2024-08-23 00:11:58 -0400

    added bandstand code to drivein

--- animator_drive_in/cody.py
+++ animator_drive_in/cody.py
@@ -15,9 +15,208 @@ import neopixel_spi
 from rainbowio import colorwheel
 import pwmio
 from adafruit_motor import servo
+import pygame
+import gc
+import files
+import utilities
+import psutil
 
-# Function to get the local IP address
+def gc_col(collection_point):
+    gc.collect()
+    start_mem = psutil.virtual_memory()[1]
+    print("Point " + collection_point +
+                   " Available memory: {} bytes".format(start_mem))
+    
+gc_col("Imports gc, files")
 
+################################################################################
+# Setup io hardware
+
+switch_io_1 = digitalio.DigitalInOut(board.D17)
+switch_io_1.direction = digitalio.Direction.INPUT
+switch_io_1.pull = digitalio.Pull.UP
+
+switch_io_2 = digitalio.DigitalInOut(board.D27)
+switch_io_2.direction = digitalio.Direction.INPUT
+switch_io_2.pull = digitalio.Pull.UP
+
+switch_io_3 = digitalio.DigitalInOut(board.D22)
+switch_io_3.direction = digitalio.Direction.INPUT
+switch_io_3.pull = digitalio.Pull.UP
+
+switch_io_4 = digitalio.DigitalInOut(board.D5)
+switch_io_4.direction = digitalio.Direction.INPUT
+switch_io_4.pull = digitalio.Pull.UP
+
+aud_en = digitalio.DigitalInOut(board.D26)
+aud_en.direction = digitalio.Direction.OUTPUT
+aud_en.value = True
+
+l_s = Debouncer(switch_io_1)
+r_s = Debouncer(switch_io_2)
+w_s = Debouncer(switch_io_3)
+b_s = Debouncer(switch_io_4)
+
+################################################################################
+# Setup sound hardware
+#i2s audio is setup on pi with an overlay
+
+# Setup the mixer to play wav files
+pygame.mixer.init()
+mix = pygame.mixer.music
+mix.load('/home/pi/Videos/left_speaker_right_speaker.wav')
+mix.set_volume(.05)
+mix.play(loops=0)
+
+#audio experiments to understand
+# pygame.mixer.init()
+# pygame.mixer.music.load('/home/pi/Videos/left_speaker_right_speaker.wav')
+# pygame.mixer.music.unload
+# pygame.mixer.music.rewind()
+# pygame.mixer.music.stop()
+# pygame.mixer.music.pause()
+# pygame.mixer.music.set_volume(0.01)
+# pygame.mixer.music.play(loops=0)
+# pos=pygame.mixer.music.get_pos()
+
+################################################################################
+# Setup video hardware
+#i2s audio is setup on pi with an overlay
+
+# create vlc media player object for playing video, music etc
+media_player = vlc.MediaPlayer()
+media_player.toggle_fullscreen()
+
+movie = []
+#movie.append("/media/pi/EMBROIDERY/Videos/Addams Family Train Wreck Scenes.mp4")
+movie.append("/home/pi/Videos/Addams Family Train Wreck Scenes.mp4")
+#movie.append("/home/pi/Videos/Baltimore & Ohio Railroad Passenger Train.mp4")
+#movie.append("/home/pi/Videos/Black River Railroad System.mp4")
+#movie.append("/home/pi/Videos/Classic Pennsy WideScreen DVD Preview.mp4")
+#movie.append("/home/pi/Videos/The Brave Locomotive.mp4")
+#movie.append("/home/pi/Videos/Toy Trains_Model Railroads.mp4")
+
+run_movie_cont = True
+volume = 20
+movie_index = 0
+media_player.audio_set_volume(volume)
+
+def play_movies():
+    global movie_index
+    if movie_index > 7: movie_index = 0
+    media = vlc.Media(movie[movie_index])
+    media_player.set_media(media)
+    play_movie()
+    movie_index +=1
+    
+def pause_movie():
+    media_player.pause()
+    
+def play_movie():
+    media_player.play()
+
+def rainbow(speed,duration):
+    startTime = time.monotonic()
+    for j in range(0,255,1):
+        for i in range(num_px):
+            pixel_index = (i * 256 // num_px) + j
+            led[i] = colorwheel(pixel_index & 255)
+        led.show()
+        time.sleep(speed)
+        timeElasped = time.monotonic()-startTime
+        if timeElasped > duration:
+            return
+    for j in reversed(range(0,255,1)):
+        for i in range(num_px):
+            pixel_index = (i * 256 // num_px) + j
+            led[i] = colorwheel(pixel_index & 255)
+        led.show()
+        time.sleep(speed)
+        timeElasped = time.monotonic()-startTime
+        if timeElasped > duration:
+            return
+        
+
+
+################################################################################
+# Setup servo hardware
+m1_pwm = pwmio.PWMOut(board.D6, duty_cycle=2 ** 15, frequency=50) #D23
+m2_pwm = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50) #D24
+m3_pwm = pwmio.PWMOut(board.D23, duty_cycle=2 ** 15, frequency=50) #D25
+m4_pwm = pwmio.PWMOut(board.D24, duty_cycle=2 ** 15, frequency=50) #D6
+m5_pwm = pwmio.PWMOut(board.D25, duty_cycle=2 ** 15, frequency=50) #D13
+m6_pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50) #D12
+m7_pwm = pwmio.PWMOut(board.D16, duty_cycle=2 ** 15, frequency=50) #D16
+m8_pwm = pwmio.PWMOut(board.D20, duty_cycle=2 ** 15, frequency=50) #D20
+
+m1_servo = servo.Servo(m1_pwm)
+m2_servo = servo.Servo(m2_pwm)
+m3_servo = servo.Servo(m3_pwm)
+m4_servo = servo.Servo(m4_pwm)
+m5_servo = servo.Servo(m5_pwm)
+m6_servo = servo.Servo(m6_pwm)
+m7_servo = servo.Servo(m7_pwm)
+m8_servo = servo.Servo(m8_pwm)
+
+m1_servo.angle = 180
+m2_servo.angle = 180
+m3_servo.angle = 180
+m4_servo.angle = 180
+m5_servo.angle = 180
+m6_servo.angle = 180
+m7_servo.angle = 180
+m8_servo.angle = 180
+
+################################################################################
+# Setup neo pixels
+num_px=10
+led = neopixel_spi.NeoPixel_SPI(board.SPI(), num_px, brightness=1.0, auto_write=False)
+
+################################################################################
+# Sd card config variables
+
+cfg = files.read_json_file("/home/pi/cfg.json")
+print(cfg)
+
+snd_opt = files.return_directory("", "/home/pi/snds", ".wav")
+
+cust_snd_opt = files.return_directory(
+    "customers_owned_music_", "/home/pi/customers_owned_music", ".wav")
+
+all_snd_opt = []
+all_snd_opt.extend(snd_opt)
+all_snd_opt.extend(cust_snd_opt)
+
+menu_snd_opt = []
+menu_snd_opt.extend(snd_opt)
+rnd_opt = ['random all', 'random built in', 'random my']
+menu_snd_opt.extend(rnd_opt)
+menu_snd_opt.extend(cust_snd_opt)
+
+ts_jsons = files.return_directory(
+    "", "/home/pi/t_s_def", ".json")
+
+web = cfg["serve_webpage"]
+
+cfg_main = files.read_json_file("/home/pi/mvc/main_menu.json")
+main_m = cfg_main["main_menu"]
+
+cfg_web = files.read_json_file("/home/pi/mvc/web_menu.json")
+web_m = cfg_web["web_menu"]
+
+cfg_vol = files.read_json_file("/home/pi/mvc/volume_settings.json")
+vol_set = cfg_vol["volume_settings"]
+
+cfg_add_song = files.read_json_file(
+    "/home/pi/mvc/add_sounds_animate.json")
+add_snd = cfg_add_song["add_sounds_animate"]
+
+cont_run = False
+ts_mode = False
+
+
+################################################################################
+# Setup wifi and web server
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,8 +230,8 @@ def get_local_ip():
         s.close()
     return ip
 
-# Define the web server handler
-
+################################################################################
+# Setup routes
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -47,7 +246,7 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 
         # Serve the HTML content from a local file
         if self.path == "/":
-            self.path = "/indexmdns.html"
+            self.path = "/index.html"
         # Set content type based on file extension
         if self.path.endswith(".css"):
             content_type = "text/css"
@@ -132,7 +331,6 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
                         "data": post_data.decode('utf-8')}
             self.wfile.write(json.dumps(response).encode('utf-8'))
 
-
 # Get the local IP address
 local_ip = get_local_ip()
 print(f"Local IP address: {local_ip}")
@@ -142,23 +340,19 @@ PORT = 8083  # Use port 80 for default HTTP access
 handler = MyHttpRequestHandler
 httpd = socketserver.TCPServer((local_ip, PORT), handler)
 
-# Function to start the web server
-
-
 def start_server():
     print(f"Serving on {local_ip}:{PORT}")
     httpd.serve_forever()
-
 
 # Set up mDNS service info
 desc = {'path': '/'}
 info = ServiceInfo(
     "_http._tcp.local.",
-    "dude._http._tcp.local.",
+    "animator-drive-in._http._tcp.local.",
     addresses=[socket.inet_aton(local_ip)],
     port=PORT,
     properties=desc,
-    server="dude.local."
+    server="animator-drive-in.local."
 )
 
 # Register mDNS service
@@ -171,118 +365,147 @@ server_thread = threading.Thread(target=start_server)
 server_thread.daemon = True
 server_thread.start()
 
-# Setup the switches, there are two the Left and Right and two unused 3 and 4
-switch_io_1 = digitalio.DigitalInOut(board.D17)
-switch_io_1.direction = digitalio.Direction.INPUT
-switch_io_1.pull = digitalio.Pull.UP
+gc_col("web server")
 
-switch_io_2 = digitalio.DigitalInOut(board.D27)
-switch_io_2.direction = digitalio.Direction.INPUT
-switch_io_2.pull = digitalio.Pull.UP
-
-switch_io_3 = digitalio.DigitalInOut(board.D22)
-switch_io_3.direction = digitalio.Direction.INPUT
-switch_io_3.pull = digitalio.Pull.UP
-
-switch_io_4 = digitalio.DigitalInOut(board.D5)
-switch_io_4.direction = digitalio.Direction.INPUT
-switch_io_4.pull = digitalio.Pull.UP
-
-aud_en = digitalio.DigitalInOut(board.D26)
-aud_en.direction = digitalio.Direction.OUTPUT
-aud_en.value = True
-
-left_switch = Debouncer(switch_io_1)
-right_switch = Debouncer(switch_io_2)
-white_switch = Debouncer(switch_io_3)
-blue_switch = Debouncer(switch_io_4)
-
-# Setup the servo
-m1_pwm = pwmio.PWMOut(board.D6, duty_cycle=2 ** 15, frequency=50)
-m2_pwm = pwmio.PWMOut(board.D13, duty_cycle=2 ** 15, frequency=50)
-m3_pwm = pwmio.PWMOut(board.D23, duty_cycle=2 ** 15, frequency=50)
-m4_pwm = pwmio.PWMOut(board.D24, duty_cycle=2 ** 15, frequency=50)
-m5_pwm = pwmio.PWMOut(board.D25, duty_cycle=2 ** 15, frequency=50)
-m6_pwm = pwmio.PWMOut(board.D12, duty_cycle=2 ** 15, frequency=50)
-m7_pwm = pwmio.PWMOut(board.D16, duty_cycle=2 ** 15, frequency=50)
-m8_pwm = pwmio.PWMOut(board.D20, duty_cycle=2 ** 15, frequency=50)
-
-m1_servo = servo.Servo(m1_pwm)
-m2_servo = servo.Servo(m2_pwm)
-m3_servo = servo.Servo(m3_pwm)
-m4_servo = servo.Servo(m4_pwm)
-m5_servo = servo.Servo(m5_pwm)
-m6_servo = servo.Servo(m6_pwm)
-m7_servo = servo.Servo(m7_pwm)
-m8_servo = servo.Servo(m8_pwm)
-
-m1_servo.angle = 180
-m2_servo.angle = 180
-m3_servo.angle = 180
-m4_servo.angle = 180
-m5_servo.angle = 180
-m6_servo.angle = 180
-m7_servo.angle = 180
-m8_servo.angle = 180
-
-# setup neopixels on spi GP10
-num_pixels_rgb=10
-ledStripRGB = neopixel_spi.NeoPixel_SPI(board.SPI(), num_pixels_rgb, brightness=1.0, auto_write=False)
+################################################################################
+# Global Methods
 
 
-# create vlc media player object for playing video, music etc
-media_player = vlc.MediaPlayer()
-media_player.toggle_fullscreen()
+def rst_def():
+    global cfg
+    cfg["volume_pot"] = True
+    cfg["HOST_NAME"] = "animator-bandstand"
+    cfg["option_selected"] = "random all"
+    cfg["volume"] = "20"
 
-movie = []
-movie.append("/media/pi/EMBROIDERY/Videos/Addams Family Train Wreck Scenes.mp4")
-#movie.append("/home/pi/Videos/Addams Family Train Wreck Scenes.mp4")
-#movie.append("/home/pi/Videos/Baltimore & Ohio Railroad Passenger Train.mp4")
-#movie.append("/home/pi/Videos/Black River Railroad System.mp4")
-#movie.append("/home/pi/Videos/Classic Pennsy WideScreen DVD Preview.mp4")
-#movie.append("/home/pi/Videos/The Brave Locomotive.mp4")
-#movie.append("/home/pi/Videos/Toy Trains_Model Railroads.mp4")
+################################################################################
+# Dialog and sound play methods
 
-run_movie_cont = False
-volume = 70
-movie_index = 0
 
-def play_movies():
-    global movie_index
-    if movie_index > 7: movie_index = 0
-    media = vlc.Media(movie[movie_index])
-    media_player.set_media(media)
-    play_movie()
-    movie_index +=1
-    
-def pause_movie():
-    media_player.pause()
-    
-def play_movie():
-    media_player.play()
+def upd_vol(seconds):
+    try:
+        volume = int(cfg["volume"]) / 100
+    except Exception as e:
+        files.log_item(e)
+        volume = .5
+    if volume < 0 or volume > 1:
+        volume = .5
+    mix.voice[0].level = volume
+    time.sleep(seconds)
 
-def rainbow(speed,duration):
-    startTime = time.monotonic()
-    for j in range(0,255,1):
-        for i in range(num_pixels_rgb):
-            pixel_index = (i * 256 // num_pixels_rgb) + j
-            ledStripRGB[i] = colorwheel(pixel_index & 255)
-        ledStripRGB.show()
-        time.sleep(speed)
-        timeElasped = time.monotonic()-startTime
-        if timeElasped > duration:
-            return
-    for j in reversed(range(0,255,1)):
-        for i in range(num_pixels_rgb):
-            pixel_index = (i * 256 // num_pixels_rgb) + j
-            ledStripRGB[i] = colorwheel(pixel_index & 255)
-        ledStripRGB.show()
-        time.sleep(speed)
-        timeElasped = time.monotonic()-startTime
-        if timeElasped > duration:
-            return
-        
-media_player.audio_set_volume(volume)
+
+def ch_vol(action):
+    v = int(cfg["volume"])
+    if "volume" in action:
+        v = action.split("volume")
+        v = int(v[1])
+    if action == "lower1":
+        v -= 1
+    elif action == "raise1":
+        v += 1
+    elif action == "lower":
+        if v <= 10:
+            v -= 1
+        else:
+            v -= 10
+    elif action == "raise":
+        if v < 10:
+            v += 1
+        else:
+            v += 10
+    if v > 100:
+        v = 100
+    if v < 1:
+        v = 1
+    cfg["volume"] = str(v)
+    cfg["volume_pot"] = False
+    files.write_json_file("/home/pi/cfg.json", cfg)
+    play_a_0("/home/pi/mvc/volume.wav")
+    spk_str(cfg["volume"], False)
+
+
+def play_a_0(file_name):
+    if mix.voice[0].playing:
+        mix.stop()
+        while mix.voice[0].playing:
+            upd_vol(0.1)
+    wave0 = audiocore.WaveFile(open(file_name, "rb"))
+    mix.voice[0].play(wave0, loop=False)
+    while mix.voice[0].playing:
+        exit_early()
+
+
+def stop_a_0():
+    mix.stop()
+    while mix.voice[0].playing:
+        pass
+
+
+def exit_early():
+    upd_vol(0.1)
+    l_sw.update()
+    if l_sw.fell:
+        mix.stop()
+
+
+def spk_str(str_to_speak, addLocal):
+    for character in str_to_speak:
+        try:
+            if character == " ":
+                character = "space"
+            if character == "-":
+                character = "dash"
+            if character == ".":
+                character = "dot"
+            play_a_0("/home/pi/mvc/" + character + ".wav")
+        except Exception as e:
+            files.log_item(e)
+            print("Invalid character in string to speak")
+    if addLocal:
+        play_a_0("/home/pi/mvc/dot.wav")
+        play_a_0("/home/pi/mvc/local.wav")
+
+
+def l_r_but():
+    play_a_0("/home/pi/mvc/press_left_button_right_button.wav")
+
+
+def sel_web():
+    play_a_0("/home/pi/mvc/web_menu.wav")
+    l_r_but()
+
+
+def opt_sel():
+    play_a_0("/home/pi/mvc/option_selected.wav")
+
+
+def spk_sng_num(song_number):
+    play_a_0("/home/pi/mvc/song.wav")
+    spk_str(song_number, False)
+
+
+def no_trk():
+    play_a_0("/home/pi/mvc/no_user_soundtrack_found.wav")
+    while True:
+        l_sw.update()
+        r_sw.update()
+        if l_sw.fell:
+            break
+        if r_sw.fell:
+            play_a_0("/home/pi/mvc/create_sound_track_files.wav")
+            break
+
+
+def spk_web():
+    play_a_0("/home/pi/mvc/animator_available_on_network.wav")
+    play_a_0("/home/pi/mvc/to_access_type.wav")
+    if cfg["HOST_NAME"] == "animator-bandstand":
+        play_a_0("/home/pi/mvc/animator_dash_bandstand.wav")
+        play_a_0("/home/pi/mvc/dot.wav")
+        play_a_0("/home/pi/mvc/local.wav")
+    else:
+        spk_str(cfg["HOST_NAME"], True)
+    play_a_0("/home/pi/mvc/in_your_browser.wav")    
 
 while True:
     if not media_player.is_playing() and run_movie_cont:
@@ -290,15 +513,15 @@ while True:
         play_movies()
         while not media_player.is_playing():
             time.sleep(.5)
-    left_switch.update()
-    right_switch.update()
-    white_switch.update()
-    blue_switch.update()
-    if left_switch.fell:
+    l_s.update()
+    r_s.update()
+    w_s.update()
+    b_s.update()
+    if l_s.fell:
         if media_player.is_playing(): pause_movie()
         run_movie_cont = True
         print ("left fell")
-    if right_switch.fell:
+    if r_s.fell:
         print ("right fell")
         if media_player.is_playing():
             run_movie_cont = False
@@ -306,12 +529,12 @@ while True:
         else:
             play_movie()
             rainbow(.005,5)
-    if white_switch.fell:
+    if w_s.fell:
         print ("white fell")
         volume = volume - 10
         if volume < 0: volume = 0
         media_player.audio_set_volume(volume)
-    if blue_switch.fell:
+    if b_s.fell:
         print ("blue fell")
         volume = volume + 10
         if volume > 100: volume = 100
@@ -319,7 +542,7 @@ while True:
     time.sleep(.1)
     pass
 
-media_player.stop()  
+media_player.stop()
 
 try:
     input("Press enter to exit...\n\n")
