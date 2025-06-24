1bedeb616b77747ac36b26f2c2c28be218bed487
Jimmysb <jbailey@jimmysoftllc.com>
2025-06-24 06:04:12 -0400

    removed func group from decoder

--- animator_dcc_encoder/simpler mulitfunction encoder.ino
+++ animator_dcc_encoder/simpler mulitfunction encoder.ino
@@ -11,7 +11,8 @@ struct CVPair {
 };
 
 CVPair FactoryDefaultCVs[] = {
-  {CV_MULTIFUNCTION_PRIMARY_ADDRESS, 3}, // Default locomotive address 3
+  {CV_ACCESSORY_DECODER_ADDRESS_LSB, DEFAULT_ACCESSORY_DECODER_ADDRESS & 0xFF},
+  {CV_ACCESSORY_DECODER_ADDRESS_MSB, DEFAULT_ACCESSORY_DECODER_ADDRESS >> 8},
 };
 
 uint8_t FactoryDefaultCVIndex = 0;
@@ -28,7 +29,6 @@ void notifyCVAck(void) {
   digitalWrite(DccAckPin, LOW);
 }
 
-// Locomotive speed and direction packet
 void notifyDccSpeed(uint16_t Addr, DCC_ADDR_TYPE AddrType, uint8_t Speed, DCC_DIRECTION Dir, DCC_SPEED_STEPS SpeedSteps) {
   Serial.print("notifyDccSpeed: Addr=");
   Serial.print(Addr, DEC);
@@ -40,14 +40,75 @@ void notifyDccSpeed(uint16_t Addr, DCC_ADDR_TYPE AddrType, uint8_t Speed, DCC_DI
   Serial.println(SpeedSteps, DEC);
 }
 
-// Locomotive function packet (F0–F28)
 void notifyDccFunc(uint16_t Addr, DCC_ADDR_TYPE AddrType, FN_GROUP FuncGrp, uint8_t FuncState) {
   Serial.print("notifyDccFunc: Addr=");
   Serial.print(Addr, DEC);
-  Serial.print(", FuncGrp=");
-  Serial.print(FuncGrp, DEC);
-  Serial.print(", FuncState=0x");
-  Serial.println(FuncState, HEX);
+  Serial.print(" (");
+  if (FuncGrp == FN_0_4) {
+    Serial.print("F0=");
+    Serial.print((FuncState & FN_BIT_00) ? "ON" : "OFF");
+    Serial.print(", F1=");
+    Serial.print((FuncState & FN_BIT_01) ? "ON" : "OFF");
+    Serial.print(", F2=");
+    Serial.print((FuncState & FN_BIT_02) ? "ON" : "OFF");
+    Serial.print(", F3=");
+    Serial.print((FuncState & FN_BIT_03) ? "ON" : "OFF");
+    Serial.print(", F4=");
+    Serial.print((FuncState & FN_BIT_04) ? "ON" : "OFF");
+  } else if (FuncGrp == FN_5_8) {
+    Serial.print("F5=");
+    Serial.print((FuncState & FN_BIT_01) ? "ON" : "OFF");
+    Serial.print(", F6=");
+    Serial.print((FuncState & FN_BIT_02) ? "ON" : "OFF");
+    Serial.print(", F7=");
+    Serial.print((FuncState & FN_BIT_03) ? "ON" : "OFF");
+    Serial.print(", F8=");
+    Serial.print((FuncState & FN_BIT_04) ? "ON" : "OFF");
+  } else if (FuncGrp == FN_9_12) {
+    Serial.print("F9=");
+    Serial.print((FuncState & FN_BIT_01) ? "ON" : "OFF");
+    Serial.print(", F10=");
+    Serial.print((FuncState & FN_BIT_02) ? "ON" : "OFF");
+    Serial.print(", F11=");
+    Serial.print((FuncState & FN_BIT_03) ? "ON" : "OFF");
+    Serial.print(", F12=");
+    Serial.print((FuncState & FN_BIT_04) ? "ON" : "OFF");
+  } else if (FuncGrp == FN_13_20) {
+    Serial.print("F13=");
+    Serial.print((FuncState & FN_BIT_01) ? "ON" : "OFF");
+    Serial.print(", F14=");
+    Serial.print((FuncState & FN_BIT_02) ? "ON" : "OFF");
+    Serial.print(", F15=");
+    Serial.print((FuncState & FN_BIT_03) ? "ON" : "OFF");
+    Serial.print(", F16=");
+    Serial.print((FuncState & FN_BIT_04) ? "ON" : "OFF");
+    Serial.print(", F17=");
+    Serial.print((FuncState & FN_BIT_05) ? "ON" : "OFF");
+    Serial.print(", F18=");
+    Serial.print((FuncState & FN_BIT_06) ? "ON" : "OFF");
+    Serial.print(", F19=");
+    Serial.print((FuncState & FN_BIT_07) ? "ON" : "OFF");
+    Serial.print(", F20=");
+    Serial.print((FuncState & FN_BIT_08) ? "ON" : "OFF");
+  } else if (FuncGrp == FN_21_28) {
+    Serial.print("F21=");
+    Serial.print((FuncState & FN_BIT_01) ? "ON" : "OFF");
+    Serial.print(", F22=");
+    Serial.print((FuncState & FN_BIT_02) ? "ON" : "OFF");
+    Serial.print(", F23=");
+    Serial.print((FuncState & FN_BIT_03) ? "ON" : "OFF");
+    Serial.print(", F24=");
+    Serial.print((FuncState & FN_BIT_04) ? "ON" : "OFF");
+    Serial.print(", F25=");
+    Serial.print((FuncState & FN_BIT_05) ? "ON" : "OFF");
+    Serial.print(", F26=");
+    Serial.print((FuncState & FN_BIT_06) ? "ON" : "OFF");
+    Serial.print(", F27=");
+    Serial.print((FuncState & FN_BIT_07) ? "ON" : "OFF");
+    Serial.print(", F28=");
+    Serial.print((FuncState & FN_BIT_08) ? "ON" : "OFF");
+  }
+  Serial.println(")");
 }
 
 void setup() {
@@ -58,7 +119,6 @@ void setup() {
   
   pinMode(DccAckPin, OUTPUT);
   Serial.println("NMRA DCC Decoder Initialized");
-  Serial.println("Init Done");
 
 #ifdef digitalPinToInterrupt
   Dcc.pin(DCC_PIN, 0);
@@ -67,6 +127,7 @@ void setup() {
 #endif
   
   Dcc.init(MAN_ID_DIY, 10, 0, 0);
+  Serial.println("Init Done");
 }
 
 void loop() {
