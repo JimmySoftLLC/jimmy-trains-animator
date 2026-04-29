1adb9b2a6594361580da9026e3222827d07517db
Jimmysb <jbailey@jimmysoftllc.com>
2026-04-29 08:34:06 -0400

    added top sw too

--- animator-CLHV-6/boot.py
+++ animator-CLHV-6/boot.py
@@ -2,15 +2,20 @@ import storage
 import board
 import digitalio
 
-# Setup the switch
+# Setup the switches
+top_sw = board.GP11
 bot_sw = board.GP20
+
+top_sw = digitalio.DigitalInOut(top_sw)
+top_sw.direction = digitalio.Direction.INPUT
+top_sw.pull = digitalio.Pull.UP
 
 bot_sw = digitalio.DigitalInOut(bot_sw)
 bot_sw.direction = digitalio.Direction.INPUT
 bot_sw.pull = digitalio.Pull.UP
 
-# Hold bottom button while plugging in / resetting to make CIRCUITPY writable by USB
-if not bot_sw.value:
+# Hold either switch while plugging in / resetting to make CIRCUITPY writable by USB
+if not top_sw.value or not bot_sw.value:
     storage.remount("/", readonly=True)    # computer can write
 else:
     storage.remount("/", readonly=False)   # code can write cfg.json
