18a87917e8c302438904eef0848e018aface9723
Jimmysb <jbailey@jimmysoftllc.com>
2026-08-26 09:38:03 -0400

    fix bug

--- animator_trolley/code.py
+++ animator_trolley/code.py
@@ -636,10 +636,9 @@ def measure_signal_strength(MY_SSID, cycles):
         if count > cycles:
             return avg_rssi
 
-if (web):
-    cycles = 10
-    avg_rssi = measure_signal_strength(WIFI_SSID, cycles)
-    print(f"Avg ({cycles} readings): {avg_rssi:.1f} dBm")
+cycles = 10
+avg_rssi = measure_signal_strength(WIFI_SSID, cycles)
+print(f"Avg ({cycles} readings): {avg_rssi:.1f} dBm")
 
 ################################################################################
 # Command queue
